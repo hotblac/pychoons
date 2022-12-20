9300fa831f1d2c4fe4bb5881551d769903126a14
Stuart 'Stevie' Leitch <hotblack@disasterarea.org.uk>
2022-12-20 21:26:07 +0000

    Extract tags for all media files in library

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
 import eyed3
+import os
+
+ROOT_DIR = '/media/music'
+MEDIA_EXTENSIONS = ['.mp3']
 
 
 def load_meta(path):
@@ -6,14 +10,35 @@ def load_meta(path):
     Load metadata for given media file
     """
     media_file = eyed3.load(path)
+    if media_file is None:
+        print(f'WARN: unrecognised file: {path}')
+        return None
     return media_file.tag
 
 
+def read_all(root):
+    """
+    Read metadata for all files under root path
+    """
+    for path, dirs, files in os.walk(root):
+        for file in files:
+            if os.path.splitext(file)[1] in MEDIA_EXTENSIONS:
+                full_path = os.path.join(path, file)
+                tag = load_meta(full_path)
+                print_tag(tag)
+
+
+def print_tag(tag):
+    if tag is not None:
+        print(f'Artist:     {tag.artist}\n'
+              f'Album:      {tag.album}\n'
+              f'Title       {tag.title}\n'
+              f'Genre:      {tag.genre}\n'
+              f'Released:   {tag.getBestDate()}')
+
+
 if __name__ == '__main__':
-    tag = load_meta('/home/stevie/Music/01 I Caputure Castles.mp3')
-    print(f'Artist:     {tag.artist}\n'
-          f'Album:      {tag.album}\n'
-          f'Genre:      {tag.genre}\n'
-          f'Released:   {tag.getBestDate()}')
+    read_all(ROOT_DIR)
+
 
 
